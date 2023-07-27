5eeefbd7ad2864912c90d0189873d1a6a4fc2f6d
pooja6747 <52853903+pooja6747@users.noreply.github.com>
2023-07-27 11:40:07 +0530

    Assignment of 25/07 and 26/07 done

---
+++ 25-07-2023/function.js
@@ -0,0 +1,78 @@
+
+//  ADDITION
+console.log("ADDITION")
+var a=10;
+var b=30;
+function addition(){
+c=a+b
+console.log(c)
+document.querySelector('.add').innerHTML = c;
+}
+
+addition()
+
+function add(a,b){
+  console.log(a+b)
+}
+
+add(1,2)
+add(5,3)
+
+
+//SUBTRACTION
+console.log("SUBTRACTION")
+var a=30;
+var b=10;
+function subtraction(){
+c=a-b
+console.log(c)
+document.querySelector('.sub').innerHTML = c;
+}
+
+subtraction()
+
+function sub(a,b){
+  console.log(a-b)
+}
+
+sub(5,2)
+sub(5,3)
+
+//MULTIPLICATION
+console.log("MULTIPLICATION")
+var a=30;
+var b=10;
+function multiplication(){
+c=a*b
+console.log(c)
+document.querySelector('.mul').innerHTML = c;
+}
+
+multiplication()
+
+function mul(a,b){
+  console.log(a*b)
+}
+
+mul(5,2)
+mul(5,3)
+
+
+//DIVISION
+console.log("DIVISION")
+var a=30;
+var b=10;
+function division(){
+c=a/b
+console.log(c)
+document.querySelector('.div').innerHTML = c;
+}
+
+division()
+
+function div(a,b){
+  console.log(a/b)
+}
+
+div(5,2)
+div(5,3)
---
+++ 26-07-2023/app.js
@@ -0,0 +1,9 @@
+function show() {
+    document.querySelector('.card-img').style.display = "block";
+}
+
+
+function showTitle(){
+    document.querySelector('.card-title').style.display = "block";
+}
+
